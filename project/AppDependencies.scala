d2e5615a06faebc60576cba70ebea06f9ef9ec85
Sandip Khedekar <17478517+SandipKhedekar@users.noreply.github.com>
2023-09-13 09:39:22 +0100

    MCSC-456 - Update Dependencies (#48)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,18 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlay28Version = "7.14.0"
+  private val bootstrapPlay28Version = "7.22.0"
+  private val mongoPlay28Version     = "1.3.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "6.8.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.12.0-play-28",
-    "uk.gov.hmrc"       %% "domain"                        % "8.1.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapPlay28Version,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "1.1.0",
-    "com.googlecode.libphonenumber" % "libphonenumber"       % "8.13.12",
-    "com.beachape" %% "enumeratum-play" % "1.6.0"
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"            % "7.19.0-play-28",
+    "uk.gov.hmrc"                  %% "play-conditional-form-mapping" % "1.13.0-play-28",
+    "uk.gov.hmrc"                  %% "domain"                        % "8.1.0-play-28",
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28"    % bootstrapPlay28Version,
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"            % mongoPlay28Version,
+    "com.googlecode.libphonenumber" % "libphonenumber"                % "8.13.12",
+    "com.beachape"                 %% "enumeratum-play"               % "1.6.0"
   )
 
   val test = Seq(
@@ -27,7 +28,7 @@ object AppDependencies {
     "com.typesafe.play"      %% "play-test"               % PlayVersion.current,
     "org.mockito"            %% "mockito-scala"           % "1.16.42",
     "org.scalacheck"         %% "scalacheck"              % "1.15.4",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "1.1.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % mongoPlay28Version,
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"
   ).map(_ % "test, it")
 
