56e21cefc95c45090a349049362db535a0b10f48
Sandip Khedekar <17478517+SandipKhedekar@users.noreply.github.com>
2023-10-04 12:25:47 +0100

    MCSC-446 - Reverted previous changes (#70)

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -45,8 +45,8 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
 
   val signOutUrl: String = configuration.get[String]("urls.signOut")
 
-  private val exitSurveyBaseUrl: String = configuration.get[String]("feedback-frontend.host")
-  val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/public-pension-adjustment"
+  private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
+  val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/submit-public-pension-adjustment-frontend"
 
   val cppaBaseUrl: String =
     configuration.get[Service]("microservice.services.calculate-public-pension-adjustment").baseUrl
