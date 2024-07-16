f09cefee968392a0decbcdb8e81d9b4c7f3ad1bd
TJSarno <thomassarno@hotmail.com>
2024-07-16 13:00:38 +0100

    MCSC-961, added unit tests and welsh placeholders

--- test/controllers/SchemeCreditConsentControllerSpec.scala
+++ test/controllers/SchemeCreditConsentControllerSpec.scala
@@ -17,9 +17,17 @@
 package controllers
 
 import base.SpecBase
+import models.{Done, SchemeCreditConsent, UserAnswers}
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
+import org.mockito.MockitoSugar.mock
+import play.api.inject
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import services.UserDataService
 import views.html.SchemeCreditConsentView
+
+import scala.concurrent.Future
 
 class SchemeCreditConsentControllerSpec extends SpecBase {
 
@@ -38,6 +46,33 @@ class SchemeCreditConsentControllerSpec extends SpecBase {
 
         status(result) mustEqual OK
         contentAsString(result) mustEqual view()(request, messages(application)).toString
+      }
+    }
+
+    "Must set SchemeCreditConsent on submit and redirect to next page" in {
+      val ua = emptyUserAnswers
+
+      val mockUserDataService = mock[UserDataService]
+
+      val userAnswersCaptor: ArgumentCaptor[UserAnswers] = ArgumentCaptor.forClass(classOf[UserAnswers])
+
+      when(mockUserDataService.set(userAnswersCaptor.capture())(any())) thenReturn Future.successful(Done)
+
+      val application = applicationBuilder(userAnswers = Some(ua), submission = Some(submission))
+        .overrides(
+          inject.bind[UserDataService].toInstance(mockUserDataService)
+        )
+        .build()
+
+      running(application) {
+        val request = FakeRequest(POST, routes.SchemeCreditConsentController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        val capturedUserAnswers = userAnswersCaptor.getValue
+        capturedUserAnswers.get(SchemeCreditConsent) mustBe Some(true)
+        redirectLocation(result).value mustEqual controllers.routes.DeclarationsController.onPageLoad.url
       }
     }
   }
