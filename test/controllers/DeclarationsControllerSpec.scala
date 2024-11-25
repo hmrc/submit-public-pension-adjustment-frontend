f952c81f2d2cb46d131a119a32a4692b3f0a8a7b
michaelaabbott <112705928+MichaelAAbbott@users.noreply.github.com>
2024-11-25 16:53:22 +0000

    MCSC-1194 Duplicate submission error

--- test/controllers/DeclarationsControllerSpec.scala
+++ test/controllers/DeclarationsControllerSpec.scala
@@ -108,6 +108,24 @@ class DeclarationsControllerSpec extends SpecBase {
       }
     }
 
+    "must redirect to submission waiting room controller when submitted " in {
+
+      val ua = emptyUserAnswers.set(ClaimOnBehalfPage, false).success.value
+
+      val application = applicationBuilder(userAnswers = Some(ua), submission = Some(submission)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, submitYourAnswerRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[DeclarationsView]
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.SubmissionWaitingRoomController.onPageLoad().url
+      }
+    }
+
     "must retrieve Pension Scheme Member Name from user answers" in {
 
       val ua = emptyUserAnswers
@@ -136,135 +154,4 @@ class DeclarationsControllerSpec extends SpecBase {
     }
   }
 
-  "must send final submission when continuing if no userSubmissionReference has already been persisted" in {
-
-    val mockCalculationInputs     = mock[CalculationInputs]
-    val mockUserDataService       = mock[UserDataService]
-    val mockSubmissionDataService = mock[SubmissionDataService]
-    val mockSubmissionService     = mock[SubmissionService]
-    val userAnswersCaptor         = ArgCaptor[UserAnswers]
-
-    when(mockUserDataService.set(any())(any())) thenReturn Future.successful(Done)
-
-    when(mockCalculationInputs.annualAllowance) thenReturn None
-    when(mockCalculationInputs.lifeTimeAllowance) thenReturn None
-    when(mockCalculationInputs.resubmission) thenReturn Resubmission(false, None)
-
-    when(mockSubmissionService.sendFinalSubmission(any, any, any, any)(any))
-      .thenReturn(Future.successful(FinalSubmissionResponse("userSubmissionReference")))
-
-    val submission: Submission =
-      Submission(
-        "id",
-        "uniqueId",
-        mockCalculationInputs,
-        Some(aCalculationResponseWithAnInDateDebitYear)
-      )
-
-    val ua: UserAnswers = completeUserAnswers
-
-    val application =
-      applicationBuilder(userAnswers = Some(ua), submission = Some(submission))
-        .overrides(
-          bind[UserDataService].toInstance(mockUserDataService),
-          bind[SubmissionDataService].toInstance(mockSubmissionDataService),
-          bind[SubmissionService].toInstance(mockSubmissionService)
-        )
-        .build()
-
-    running(application) {
-      val request = FakeRequest(GET, submitYourAnswerRoute)
-
-      val result = route(application, request).value
-
-      status(result) mustEqual SEE_OTHER
-      redirectLocation(result).value mustEqual routes.SubmissionController.onPageLoad().url
-
-      verify(mockUserDataService).set(userAnswersCaptor)(any())
-      val capturedUserAnswers: UserAnswers = userAnswersCaptor.value
-      capturedUserAnswers.get(UserSubmissionReference()).get mustEqual "userSubmissionReference"
-    }
-  }
-
-  "must not re-send final submission when continuing if userSubmissionReference has already been persisted" in {
-
-    val mockCalculationInputs     = mock[CalculationInputs]
-    val mockUserDataService       = mock[UserDataService]
-    val mockSubmissionDataService = mock[SubmissionDataService]
-    val mockSubmissionService     = mock[SubmissionService]
-
-    when(mockUserDataService.set(any())(any())) thenReturn Future.successful(Done)
-
-    when(mockCalculationInputs.annualAllowance) thenReturn None
-    when(mockCalculationInputs.lifeTimeAllowance) thenReturn None
-    when(mockCalculationInputs.resubmission) thenReturn Resubmission(false, None)
-
-    val submission: Submission =
-      Submission(
-        "id",
-        "uniqueId",
-        mockCalculationInputs,
-        Some(aCalculationResponseWithAnInDateDebitYear)
-      )
-
-    val uaWithUserSubmissionReference: UserAnswers =
-      completeUserAnswers.set(UserSubmissionReference(), "userSubmissionReference").get
-
-    val application =
-      applicationBuilder(userAnswers = Some(uaWithUserSubmissionReference), submission = Some(submission))
-        .overrides(
-          bind[UserDataService].toInstance(mockUserDataService),
-          bind[SubmissionDataService].toInstance(mockSubmissionDataService),
-          bind[SubmissionService].toInstance(mockSubmissionService)
-        )
-        .build()
-
-    running(application) {
-      val request = FakeRequest(GET, submitYourAnswerRoute)
-
-      val result = route(application, request).value
-
-      status(result) mustEqual SEE_OTHER
-      redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
-      verifyNoInteractions(mockSubmissionService)
-    }
-  }
-
-  private def completeUserAnswers = {
-    val ua = UserAnswers(userAnswersId)
-      .set(ClaimOnBehalfPage, false)
-      .success
-      .value
-      .set(WhoWillPayPage(Period._2020), WhoWillPay.You)
-      .success
-      .value
-      .set(WhoWillPayPage(Period._2021), WhoWillPay.PensionScheme)
-      .success
-      .value
-      .set(WhichPensionSchemeWillPayPage(Period._2021), "Private pension scheme")
-      .success
-      .value
-      .set(PensionSchemeDetailsPage(Period._2021), PensionSchemeDetails("name", "pstr"))
-      .success
-      .value
-      .set(AskedPensionSchemeToPayTaxChargePage(Period._2021), true)
-      .success
-      .value
-      .set(WhenDidYouAskPensionSchemeToPayPage(Period._2021), LocalDate.of(2020, 1, 1))
-      .success
-      .value
-      .set(WhoWillPayPage(Period._2022), WhoWillPay.PensionScheme)
-      .success
-      .value
-      .set(WhichPensionSchemeWillPayPage(Period._2022), "Scheme1_PSTR")
-      .success
-      .value
-      .set(AskedPensionSchemeToPayTaxChargePage(Period._2022), false)
-      .success
-      .value
-      .set(WhenWillYouAskPensionSchemeToPayPage(Period._2022), WhenWillYouAskPensionSchemeToPay.OctToDec23)
-      .success
-      .value
-    ua
-  }
 }
