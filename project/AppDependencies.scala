ac1d5686b85453753fe37ce67b48e3d89c3b7715
Michael Abbott <112705928+MichaelAAbbott@users.noreply.github.com>
2025-03-06 16:04:26 +0000

    MCSC-1395 Update libraries (#228)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,20 @@ import sbt.*
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlay30Version = "9.7.0"
-  private val mongoPlay30Version     = "2.4.0"
+  private val bootstrapPlay30Version = "9.11.0"
+  private val mongoPlay30Version     = "2.5.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
 
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"    % "11.9.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"    % "11.12.0",
     "uk.gov.hmrc"                  %% "play-conditional-form-mapping-play-30" % "3.2.0",
     "uk.gov.hmrc"                  %% "domain-play-30"                        % "10.0.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30"    % bootstrapPlay30Version,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"            % mongoPlay30Version,
     "com.googlecode.libphonenumber" % "libphonenumber"                % "8.13.34",
     "com.beachape"                 %% "enumeratum-play"               % "1.8.0",
-    "uk.gov.hmrc"                  %% "crypto-json-play-30"           % "8.1.0"
+    "uk.gov.hmrc"                  %% "crypto-json-play-30"           % "8.2.0"
   )
 
   val test = Seq(
