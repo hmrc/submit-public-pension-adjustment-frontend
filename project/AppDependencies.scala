cc026186deec8fb635b48629b1876ee391d2a035
jmsatheesh <19515164+jmsatheesh@users.noreply.github.com>
2026-01-08 09:46:44 +0000

    Dependency upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,19 +3,19 @@ import sbt.*
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlay30Version = "10.4.0"
-  private val mongoPlay30Version     = "2.10.0"
+  private val bootstrapPlay30Version = "10.5.0"
+  private val mongoPlay30Version     = "2.11.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
 
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"            % "12.21.0",
-    "uk.gov.hmrc"                   %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"            % "12.25.0",
+    "uk.gov.hmrc"                   %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"                   %% "domain-play-30"                        % "13.0.0",
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"     % bootstrapPlay30Version,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"             % mongoPlay30Version,
-    "com.googlecode.libphonenumber" % "libphonenumber"                  % "9.0.16",
-    "com.beachape"                  %% "enumeratum-play"                % "1.9.0",
+    "com.googlecode.libphonenumber" % "libphonenumber"                  % "9.0.21",
+    "com.beachape"                  %% "enumeratum-play"                % "1.9.2",
     "uk.gov.hmrc"                   %% "crypto-json-play-30"            % "8.4.0"
   )
 
@@ -25,9 +25,8 @@ object AppDependencies {
     "org.scalacheck"         %% "scalacheck"              % "1.19.0" % Test,
     "org.scalatestplus"      %% "scalacheck-1-17"         % "3.2.18.0" % Test,
     "org.scalatestplus"      %% "mockito-4-11"            % "3.2.18.0" % Test,
-    "org.jsoup"               % "jsoup"                   % "1.21.2" % Test,
+    "org.jsoup"               % "jsoup"                   % "1.22.1" % Test,
     "org.playframework"      %% "play-test"               % PlayVersion.current % Test,
-    "org.scalacheck"         %% "scalacheck"              % "1.19.0" % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoPlay30Version % Test,
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.8" % Test
   )
