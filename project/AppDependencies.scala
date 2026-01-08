1d4f417452f5846682a9a54dc6fb724de8eebdd3
jmsatheesh <19515164+jmsatheesh@users.noreply.github.com>
2026-01-08 11:54:06 +0000

    Dependency upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,17 +15,14 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"     % bootstrapPlay30Version,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"             % mongoPlay30Version,
     "com.googlecode.libphonenumber" % "libphonenumber"                  % "9.0.21",
-    "com.beachape"                  %% "enumeratum-play"                % "1.9.2",
     "uk.gov.hmrc"                   %% "crypto-json-play-30"            % "8.4.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapPlay30Version % Test,
-    "org.scalatest"          %% "scalatest"               % "3.2.19" % Test,
     "org.scalacheck"         %% "scalacheck"              % "1.19.0" % Test,
     "org.scalatestplus"      %% "scalacheck-1-17"         % "3.2.18.0" % Test,
     "org.scalatestplus"      %% "mockito-4-11"            % "3.2.18.0" % Test,
-    "org.jsoup"               % "jsoup"                   % "1.22.1" % Test,
     "org.playframework"      %% "play-test"               % PlayVersion.current % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoPlay30Version % Test,
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.8" % Test
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -11,16 +11,16 @@ addSbtPlugin("org.playframework" % "sbt-plugin" % "3.0.9")
 
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0"  exclude("org.scala-lang.modules", "scala-xml_2.12"))
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.3.0")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.3.1")
 
 addSbtPlugin("io.github.irundaia" % "sbt-sassify" % "1.5.2")
 
 addSbtPlugin("com.github.sbt" % "sbt-concat" % "1.0.0")
 
-addSbtPlugin("com.github.sbt" % "sbt-uglify" % "3.0.0")
+addSbtPlugin("com.github.sbt" % "sbt-uglify" % "3.0.1")
 
-addSbtPlugin("com.github.sbt" % "sbt-digest" % "2.0.0")
+addSbtPlugin("com.github.sbt" % "sbt-digest" % "2.1.0")
 
-addSbtPlugin("de.heikoseeberger" % "sbt-header" % "5.6.0")
+addSbtPlugin("de.heikoseeberger" % "sbt-header" % "5.10.0")
 
-addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.6")
