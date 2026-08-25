7e32a663a29bf001682ad8b7ad07890002c3a367
jmsatheesh <19515164+jmsatheesh@users.noreply.github.com>
2026-08-25 10:51:43 +0100

    Dependency upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,24 +3,23 @@ import sbt.*
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlay30Version = "10.7.0"
-  private val mongoPlay30Version     = "2.12.0"
+  private val bootstrapPlay30Version = "10.8.0"
+  private val mongoPlay30Version     = "2.13.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
 
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"            % "13.3.0",
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"            % "13.11.0",
     "uk.gov.hmrc"                   %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"                   %% "domain-play-30"                        % "13.0.0",
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"     % bootstrapPlay30Version,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"             % mongoPlay30Version,
-    "com.googlecode.libphonenumber" % "libphonenumber"                  % "9.0.28",
+    "com.googlecode.libphonenumber" % "libphonenumber"                  % "9.0.37",
     "uk.gov.hmrc"                   %% "crypto-json-play-30"            % "8.4.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapPlay30Version % Test,
-    "org.scalacheck"         %% "scalacheck"              % "1.19.0" % Test,
     "org.scalatestplus"      %% "scalacheck-1-17"         % "3.2.18.0" % Test,
     "org.scalatestplus"      %% "mockito-4-11"            % "3.2.18.0" % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoPlay30Version % Test,
